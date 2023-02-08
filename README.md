d1e62fe47dd30f6b12fcf340fe8c1b39cdf56064
Jamie Scott <jamie@endor.ai>
2023-02-07 21:25:56 -0800

    publish github action

---
+++ README.md
@@ -0,0 +1,78 @@
+# Endor Labs Repository Scan Action
+
+Endor Labs helps developers spend less time dealing with security issues and more time accelerating development through safe Open Source Software (OSS) adoption. Our Dependency Lifecycle Management™ Solution helps organizations maximize software reuse by enabling security and development teams to select, secure, and maintain OSS at scale.
+
+The Endor Labs GitHub action may be used to repeatably integrate Endor Labs scanning jobs into your CI pipelines.
+
+## Required Parameters and Pre-requisites
+
+The following pre-requisites are required for the Endor Labs GitHub action to successfully run:
+
+- The GitHub action must be able to authenticate to the Endor Labs API. It may authenticate through either:
+  - An Endor Labs API key and secret
+  - A GCP service account with workload identity federation enabled associated with the runner
+- The Endor Labs namespace to authenticate against
+- Access to the Endor Labs API at https://api.endorlabs.com
+
+Here is an example of using Endor Labs as a scan step in a job with these pre-requisites:
+
+```
+      - name: Example step to scan with Endor Labs
+        uses: endorlabs/github-action@main
+        with:
+          api_key: ${{ secrets.ENDOR_API_KEY }}
+          api_secret: ${{ secrets.ENDOR_API_SECRET }}
+          namespace: "testing-tenant"
+```
+
+## High Level Usage Steps
+
+1. Setup authentication to Endor Labs
+   1. If you are using an API Key and Secret add these as repository secrets
+   2. If you are using GCP workload identity federation ensure you are using a self-hosted runner in GCP with your service account setup for workload identity federation.
+2. Checkout your code
+3. Install your build toolchain
+4. Build your code
+5. Scan with Endor Labs
+
+Below is an example workflow to scan with Endor Labs for a Java application.
+
+```
+name: Endor Labs Example
+on: push
+jobs:
+  scan:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Scan with Endor Labs
+      - uses: actions/checkout@v3
+      - uses: actions/setup-java@v3
+        with:
+          distribution: 'microsoft'
+          java-version: '17'
+      - name: Compile Package
+        run: mvn clean install
+      - name: Scan with Endor Labs
+        uses: endorlabs/github-action@main
+        with:
+          api_key: ${{ secrets.ENDOR_API_KEY }}
+          api_secret: ${{ secrets.ENDOR_API_SECRET }}
+          namespace: "example"
+```
+## Supported Configuration Parameters
+
+The following input parameters are supported configurations for the Endor Labs GitHub action:
+
+| Flags                                 |  Description                       |
+|---------------------------------------|------------------------------------|
+|  `api_key`                            |  Set to your Endor Labs API key ID |
+|  `api_secret`                         |  Set to your Endor Labs API key secret |
+| `gcp_service_account`                 |  Set to the GCP service account used for workload identity federation. This may not be used in conjunction with your API key   |
+| `namespace`                           | Set to your Endor Labs namespace (Required) |
+| `endorctl_version`                    | Set to a specified version of endorctl to pin this specific version for use. If this is not used, then the latest version of endorctl will be downloaded for use |
+| `endorctl_checksum`                   | Set to the checksum associated with a pinned version of endorctl. |
+| `show_progress`                       | Set to true to show a progress bar for your Endor Labs scan. Defaults to false |
+| `log_verbose`                         | Set to true to enable verbose logging mode |
+| `log_level`                           | Set to debug to enable debug logging or error to enable error logging only. Defaults to info |
+| `scan_summary_output_type`            | Set the desired output summary to table, json, or yaml |
+| `additional_args`                     | Use additional this input to add custom arguments to your endorctl command |
---
+++ src/scan.ts
@@ -0,0 +1,160 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as github from "@actions/github";
+import * as httpm from "@actions/http-client";
+import * as io from "@actions/io";
+import * as tc from "@actions/tool-cache";
+import * as path from "path";
+import * as crypto from "crypto";
+import * as fs from "fs";
+
+interface SetupProps {
+  version: string;
+  checksum: string;
+  api: string;
+}
+
+const execOptionSilent = {
+  silent: true,
+};
+
+const createHashFromFile = (filePath: string) =>
+  new Promise((resolve) => {
+    const hash = crypto.createHash("sha256");
+    fs.createReadStream(filePath)
+      .on("data", (data) => hash.update(data))
+      .on("end", () => resolve(hash.digest("hex")));
+  });
+
+const setupEndorctl = async ({ version, checksum, api }: SetupProps) => {
+  const _http: httpm.HttpClient = new httpm.HttpClient("endor-http-client");
+
+  try {
+    let endorctlVersion = version;
+    let endorctlChecksum = checksum;
+    if (!version) {
+      core.info(`Endorctl version not provided, using latest version`);
+      const res: httpm.HttpClientResponse = await _http.get(
+        `${api}/meta/version`
+      );
+      const body: string = await res.readBody();
+      const obj = JSON.parse(body);
+      endorctlVersion = obj?.Service?.Version;
+      endorctlChecksum = obj?.ClientChecksums?.ARCH_TYPE_LINUX_AMD64;
+    }
+
+    core.info(`Downloading endorctl version ${endorctlVersion}`);
+    let url = `https://storage.googleapis.com/endorlabs/${endorctlVersion}/binaries/endorctl_${endorctlVersion}_linux_amd64`;
+    let downloadPath: string | null = null;
+
+    downloadPath = await tc.downloadTool(url);
+    const hash = await createHashFromFile(downloadPath);
+    if (hash !== endorctlChecksum) {
+      throw new Error(
+        "The checksum of the downloaded binary does not match the expected value!"
+      );
+    } else {
+      core.info(`Binary checksum: ${endorctlChecksum}`);
+    }
+
+    await exec.exec("chmod", ["+x", downloadPath], execOptionSilent);
+    const binPath = ".";
+    const endorctlPath = path.join(binPath, "endorctl");
+    await io.mv(downloadPath, endorctlPath);
+    core.addPath(binPath);
+
+    core.info(`Endorctl downloaded and added to the path`);
+  } catch (error: any) {
+    core.setFailed(error);
+  }
+};
+
+async function run() {
+  let scanResult = "";
+
+  const scanOptions: exec.ExecOptions = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        scanResult += data.toString();
+      },
+      stderr: (data: Buffer) => {
+        scanResult += data.toString();
+      },
+    },
+    ...execOptionSilent,
+  };
+
+  try {
+    const API = core.getInput("api");
+    const API_KEY = core.getInput("api_key");
+    const API_SECRET = core.getInput("api_secret");
+    const GCP_CREDENTIALS_SERVICE_ACCOUNT = core.getInput(
+      "gcp_service_account"
+    );
+    const NAMESPACE = core.getInput("namespace");
+    const ENDORCTL_VERSION = core.getInput("endorctl_version");
+    const ENDORCTL_CHECKSUM = core.getInput("endorctl_checksum");
+    const SHOW_PROGRESS = core.getBooleanInput("show_progress");
+    const LOG_VERBOSE = core.getBooleanInput("log_verbose");
+    const LOG_LEVEL = core.getInput("log_level");
+    const SCAN_SUMMARY_OUTPUT_TYPE = core.getInput("scan_summary_output_type");
+    const ADDITIONAL_ARGS = core.getInput("additional_args");
+
+    core.info(`Endor Namespace: ${NAMESPACE}`);
+
+    if (!NAMESPACE) {
+      core.setFailed(
+        "namespace is required and must be passed as an input from the workflow"
+      );
+      return;
+    }
+    if (!(API_KEY && API_SECRET) && !GCP_CREDENTIALS_SERVICE_ACCOUNT) {
+      core.setFailed(
+        "Authentication info not found. Either a gcp service account or api key and secret combination must be passed as an input from the workflow"
+      );
+      return;
+    }
+    await setupEndorctl({
+      version: ENDORCTL_VERSION,
+      checksum: ENDORCTL_CHECKSUM,
+      api: API,
+    });
+
+    const repoName = github.context.repo.repo;
+
+    core.info(`Scanning repository ${repoName}`);
+
+    const options = [
+      `--namespace=${NAMESPACE}`,
+      `--show-progress=${SHOW_PROGRESS}`,
+      `--verbose=${LOG_VERBOSE}`,
+      `--output-type=${SCAN_SUMMARY_OUTPUT_TYPE}`,
+      `--log-level=${LOG_LEVEL}`,
+    ];
+
+    if (API) options.push(`--api=${API}`);
+    if (API_KEY && API_SECRET)
+      options.push(`--api-key=${API_KEY}`, `--api-secret=${API_SECRET}`);
+    if (GCP_CREDENTIALS_SERVICE_ACCOUNT)
+      options.push(`--gcp-service-account=${GCP_CREDENTIALS_SERVICE_ACCOUNT}`);
+
+    if (ADDITIONAL_ARGS) {
+      options.push(ADDITIONAL_ARGS);
+    }
+
+    await exec.exec(
+      `endorctl scan --path=. ${options.join(" ")}`,
+      [],
+      scanOptions
+    );
+
+    core.info(`Scan Result:`);
+    core.info(scanResult);
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
+export {};
